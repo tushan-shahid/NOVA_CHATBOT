9160f1de3388b247c219f7ad31b5c93207be24f0
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-04-09 18:50:13 -0700

    Create Synonyms.java

---
+++ src/Synonyms.java
@@ -0,0 +1,34 @@
+import rita.*;
+import rita.RiWordNet;
+import java.util.*;
+
+public class Synonyms {
+
+	String[] allSynonyms;
+	List<String> synonyms = new ArrayList<String>();
+//	static String[] stemmedSynonyms;
+	
+
+	public List<String> getSynonyms(String word) 
+	{
+		//load wordnet
+		RiWordNet wordnet;
+	    wordnet = new RiWordNet("./dict");
+	    String[] poss = wordnet.getPos(word);
+	    //loops through all synonyms
+	    for (int j = 0; j < poss.length; j++) {
+	        allSynonyms = wordnet.getAllSynonyms(word,poss[0]);
+	        synonyms = Arrays.asList(allSynonyms);
+//	        for (int i = 0; i < synonyms.length; i++) {
+//	            stemmedSynonyms[i] = RiTa.stem(synonyms[i]);
+//	        }
+	    }
+	    return synonyms;
+	}
+//	public static void main(String args[])
+//	{
+//	    Synonyms w = new Synonyms();
+//	    System.out.println(w.getSynonyms("mad"));
+//
+//	}
+}

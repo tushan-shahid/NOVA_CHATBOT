58a1922eb6c6db0b4b1fa506bbf598b4a7593ed4
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-04-09 18:55:07 -0700

    Create POSTaggerTest.java

---
+++ testing/POSTaggerTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import POSTagger;
+
+class POSTaggerTest {
+
+	@Test
+	void testReturnString() { //tests if returns complete string of Adjectives
+		POSTagger in = new POSTagger("I am feeling Happy and Excited today!");//Sample input
+		String[] desiredOutput = {"Happy", "Excited"}; //Desired output from input
+		assertArrayEquals(in.getTaggedWords(), desiredOutput);//Checks if Class returns the correct output
+	}
+	
+
+}

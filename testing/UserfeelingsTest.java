809b4f7d2a50b7a953c5b4f8a6933add7f7b595f
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-04-09 18:55:32 -0700

    Create UserfeelingsTest.java

---
+++ testing/UserfeelingsTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import POSTagger;
+import Userfeelings;
+
+class UserfeelingsTest {
+Userfeelings uf = new Userfeelings(); 
+
+	@Test // Fail
+	void testGetEmotion() {
+	String sent = "I am Very mad";
+	POSTagger tw = new POSTagger(sent);
+		uf.findEmotion(tw.getTaggedWords());	
+		
+		assertEquals("sad", uf.getEmotion());
+		
+	}
+@Test // pass
+void testGetEmotion1() {
+	
+	String sent = "I am Very mad";
+	POSTagger tw = new POSTagger(sent);
+	uf.findEmotion(tw.getTaggedWords());
+		
+		assertEquals("mad", uf.getEmotion());
+		
+	}
+
+}
+
+
+
